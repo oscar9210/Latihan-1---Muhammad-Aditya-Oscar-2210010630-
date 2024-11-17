96529a5a373e5ed1f85f68d4f278c415fdb96561
AurynNajla <aurynn18@gmail.com>
2024-11-17 22:05:17 +0800

    First Commit

---
+++ src/main/java/com/Auryn/latihan1pbo/Latihan1PBO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.Auryn.latihan1pbo;
+
+/**
+ *
+ * @author User
+ */
+public class Latihan1PBO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
